fe47b83f30fd1bb6793f44c29e77a9e228c414b2
Prasad Kawthekar <prasadkawthekar1@gmail.com>
2017-06-01 05:26:12 +0000

    thor challenge accepted

---
+++ dagger_constants.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+LOCAL_T_MAX = 5 # repeat step size
+RMSP_ALPHA = 0.99 # decay parameter for RMSProp
+RMSP_EPSILON = 0.1 # epsilon parameter for RMSProp
+CHECKPOINT_DIR = 'checkpoints'
+LOG_FILE = 'logs'
+INITIAL_ALPHA_LOW = 1e-4    # log_uniform low limit for learning rate
+INITIAL_ALPHA_HIGH = 1e-2   # log_uniform high limit for learning rate
+
+ACTION_SIZE = 4 # action size
+
+INITIAL_ALPHA_LOG_RATE = 0.4226 # log_uniform interpolate rate for learning rate (around 7 * 10^-4)
+GAMMA = 0.99 # discount factor for rewards
+ENTROPY_BETA = 0.01 # entropy regurarlization constant
+MAX_TIME_STEP = 10.0 * 10**6 # 10 million frames
+GRAD_NORM_CLIP = 40.0 # gradient norm clipping
+USE_GPU = True # To use GPU, set True
+VERBOSE = True
+
+SCREEN_WIDTH = 84
+SCREEN_HEIGHT = 84
+HISTORY_LENGTH = 4
+
+NUM_EVAL_EPISODES = 100 # number of episodes for evaluation
+
+TASK_TYPE = 'navigation' # no need to change
+
+# DAGGER IMITATION LEARNING SPECIFIC
+
+PARALLEL_SIZE = 1 # parallel thread size
+TASK_LIST = {
+  'bathroom_02'    : ['26']
+}
---
+++ dagger_train.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+import threading
+import numpy as np
+
+import signal
+import random
+import os
+
+from dagger_policy_generator import SmashNet
+from dagger_training_thread import SmashNetTrainingThread
+
+from utils.ops import log_uniform
+from utils.rmsprop_applier import RMSPropApplier
+
+from dagger_constants import ACTION_SIZE, PARALLEL_SIZE, INITIAL_ALPHA_LOW, INITIAL_ALPHA_HIGH, INITIAL_ALPHA_LOG_RATE, MAX_TIME_STEP, CHECKPOINT_DIR, LOG_FILE, RMSP_EPSILON, RMSP_ALPHA, GRAD_NORM_CLIP, USE_GPU, TASK_TYPE, TASK_LIST
+
+if __name__ == '__main__':
+
+  device = "/gpu:0" if USE_GPU else "/cpu:0"
+  network_scope = TASK_TYPE
+  list_of_tasks = TASK_LIST # single scene, single target for now
+  scene_scopes = list_of_tasks.keys()
+  global_t = 0
+  stop_requested = False
+
+  if not os.path.exists(CHECKPOINT_DIR): os.mkdir(CHECKPOINT_DIR)
+
+  initial_learning_rate = log_uniform(INITIAL_ALPHA_LOW,
+                                      INITIAL_ALPHA_HIGH,
+                                      INITIAL_ALPHA_LOG_RATE)
+
+  global_network = SmashNet(action_size = ACTION_SIZE,
+                                        device = device,
+                                        network_scope = network_scope,
+                                        scene_scopes = scene_scopes)
+
+  branches = []
+  for scene in scene_scopes:
+    for task in list_of_tasks[scene]:
+      branches.append((scene, task)) # single scene, task pair for now
+
+  NUM_TASKS = len(branches)
+  assert PARALLEL_SIZE >= NUM_TASKS, "Not enough threads for multitasking: at least {} threads needed.".format(NUM_TASKS)
+
+  learning_rate_input = tf.placeholder("float")
+  grad_applier = RMSPropApplier(learning_rate = learning_rate_input,
+                                decay = RMSP_ALPHA,
+                                momentum = 0.0,
+                                epsilon = RMSP_EPSILON,
+                                clip_norm = GRAD_NORM_CLIP,
+                                device = device)
+
+  # instantiate each training thread
+  # each thread is training for one target in one scene
+  training_threads = [] # 1 training thread for the single target
+  for i in range(PARALLEL_SIZE):
+    scene, task = branches[i%NUM_TASKS]
+    training_thread = SmashNetTrainingThread(i, global_network, initial_learning_rate,
+                                        learning_rate_input,
+                                        grad_applier, MAX_TIME_STEP,
+                                        device = device,
+                                        network_scope = "thread-%d"%(i+1),
+                                        scene_scope = scene,
+                                        task_scope = task)
+    training_threads.append(training_thread)
+
+  # prepare session
+  sess = tf.Session(config=tf.ConfigProto(log_device_placement=False,
+                                          allow_soft_placement=True))
+
+  init = tf.global_variables_initializer()
+  sess.run(init)
+
+  # create tensorboard summaries
+  summary_op = dict()
+  summary_placeholders = dict()
+
+  for i in range(PARALLEL_SIZE):
+    scene, task = branches[i%NUM_TASKS]
+    key = scene + "-" + task
+
+    # summary for tensorboard
+    episode_length_input = tf.placeholder("float")
+
+    scalar_summaries = [
+      tf.summary.scalar(key+"/Episode Length", episode_length_input),
+    ]
+
+    summary_op[key] = tf.summary.merge(scalar_summaries)
+    summary_placeholders[key] = {
+      "episode_length_input": episode_length_input,
+      "learning_rate_input": learning_rate_input
+    }
+
+  summary_writer = tf.summary.FileWriter(LOG_FILE, sess.graph)
+
+  # init or load checkpoint with saver
+  # if you don't need to be able to resume training, use the next line instead.
+  # it will result in a much smaller checkpoint file.
+  # saver = tf.train.Saver(max_to_keep=10, var_list=global_network.get_vars())
+  saver = tf.train.Saver(max_to_keep=10)
+
+  checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
+  if checkpoint and checkpoint.model_checkpoint_path:
+    saver.restore(sess, checkpoint.model_checkpoint_path)
+    print("checkpoint loaded: {}".format(checkpoint.model_checkpoint_path))
+    tokens = checkpoint.model_checkpoint_path.split("-")
+    # set global step
+    global_t = int(tokens[1])
+    print(">>> global step set: {}".format(global_t))
+  else:
+    print("Could not find old checkpoint")
+
+
+  def train_function(parallel_index):
+    global global_t
+    training_thread = training_threads[parallel_index]
+    last_global_t = 0
+
+    scene, task = branches[parallel_index % NUM_TASKS]
+    key = scene + "-" + task
+
+    while global_t < MAX_TIME_STEP and not stop_requested:
+      diff_global_t = training_thread.process(sess, global_t, summary_writer,
+                                              summary_op[key], summary_placeholders[key])
+      global_t += diff_global_t
+      # periodically save checkpoints to disk
+      if parallel_index == 0 and global_t - last_global_t > 1000000:
+        print('Save checkpoint at timestamp %d' % global_t)
+        saver.save(sess, CHECKPOINT_DIR + '/' + 'checkpoint', global_step = global_t)
+        last_global_t = global_t
+
+  def signal_handler(signal, frame):
+    global stop_requested
+    print('You pressed Ctrl+C!')
+    stop_requested = True
+
+  train_threads = [threading.Thread(target=train_function, args=(i,)) for i in range(PARALLEL_SIZE)]
+  signal.signal(signal.SIGINT, signal_handler)
+  # start each training thread
+  for t in train_threads: t.start()
+
+  print('Press Ctrl+C to stop.')
+  signal.pause()
+
+  # wait for all threads to finish
+  for t in train_threads: t.join()
+
+  print('Now saving data. Please wait.')
+  saver.save(sess, CHECKPOINT_DIR + '/' + 'checkpoint', global_step = global_t)
+  summary_writer.close()
